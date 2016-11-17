d4378518d1a847bbc39bdef93bd227d029cba4f2
KevinMcK95 <KevinMcK95@shaw.ca>
2016-11-16 20:11:35 -0500

    matrixMult.c created and takes input.

---
+++ matrixMult.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int l1, l2, w1, w2;
+	printf("Please enter the length and width of the first matrix:\n");
+	scanf("%d %d",&l1,&w1);
+	printf("Your length is %d and your width is %d.\n",l1,w1);
+	return 0;
+
+}
