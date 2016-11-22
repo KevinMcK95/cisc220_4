b835438959418c759676193f7ccf325b6110ac90
KevinMcK95 <KevinMcK95@shaw.ca>
2016-11-22 00:42:06 -0500

    capitalizer.c seems to be fully functional

--- capitalizer.c
+++ capitalizer.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h> //for the toupper(char) function
 
 int main(int argc, char **argv)
 {
 	FILE *fp = fopen(argv[1],"r+");
-	char buff[10000], newBuff[10000];
-	while (1) {
-		fgets(buff, 10000, fp);
-		if ( feof(fp) ) {
-			break;
+	char c;
+	int capTest = -1;
+	while ((c = fgetc(fp)) != EOF) {
+		if (capTest == -1){
+			c = toupper(c);
+			capTest = 0;
 		}
-		printf("%s",buff);
+		if (c == ' ')
+			capTest++;
+		if (capTest == 2)
+			capTest = -1;
+		if (c=='\n'){
+			capTest = -1;
+		}
+		fseek(fp, -1, SEEK_CUR); //move back 1 place to replace the char
+		fprintf(fp,"%c",c);
 	}
 	fclose(fp);
 	return 0;
